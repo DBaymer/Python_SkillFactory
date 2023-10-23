3082b7e5cece13d772149dad977a2cd39df7e659
Sapozhnikov Dmitriy <SapozhnikovDL@alfastrah.ru>
2023-10-23 13:10:35 +0500

    Fixing variable to input

--- B/B4/4.2.5.py
+++ B/B4/4.2.5.py
@@ -1,16 +1,12 @@
 # Найти количество делителей для числа a:
-c = int(input("Введите число:"))
-
+a = int(input("Введите число: "))
 
 def get_multipliers(a):
     count = 0  # Счётчик по умолчанию = 0
     for n in range(1, a + 1):  # Последовательность от 1 до a, последнее число не включается
         if a % n == 0:
             count += 1
-
-    return count  # Почему-то не выводит значение
-    # print(count)
+    return count
 
 
-t = get_multipliers(5)
-print(t)
+print(get_multipliers(a))
