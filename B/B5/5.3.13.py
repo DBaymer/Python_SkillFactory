851b8397ddf048cb66bbff0d77a9990c67278e46
Sapozhnikov Dmitriy <SapozhnikovDL@alfastrah.ru>
2023-11-15 11:02:49 +0500

    Add to examples tuple and matrix

--- B/B5/5.3.13.py
+++ B/B5/5.3.13.py
@@ -1,3 +1,11 @@
 # При помощи генератора списков создать таблицу умножения чисел от 1 до 10
 T = [[i*j for j in range(1, 11)] for i in range(1, 11)]
 print(T)
+
+# Можно составить список из кортежей
+list_tuples = [(i, i**2) for i in range(1,11)]
+print(list_tuples)
+
+# Можно создать матрицу «одним махом»
+M = [[i+j for j in range(5)] for i in range(5)]
+print(M)
